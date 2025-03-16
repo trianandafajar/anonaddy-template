46231c1a36c41485c3d5d70699998b258320d094
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-16 21:09:08 +0700

    Update Format Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,8 @@
                         <div class="mx-auto mt-6 w-24 border-b-2 border-grey-200"></div>
 
                         @if (session('status'))
-                            <div class="text-sm border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mt-4" role="alert">
+                            <div class="text-sm border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mt-4"
+                                role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
@@ -32,7 +33,9 @@
                                 {{ __('Username') }}:
                             </label>
 
-                            <input id="username" type="text" class="appearance-none bg-grey-100 rounded w-full p-3 text-grey-700 focus:ring{{ $errors->has('username') ? ' border-red-500' : '' }}" name="username" value="{{ old('username') }}" placeholder="johndoe" required autofocus>
+                            <input id="username" type="text"
+                                class="appearance-none bg-grey-100 rounded w-full p-3 text-grey-700 focus:ring{{ $errors->has('username') ? ' border-red-500' : '' }}"
+                                name="username" value="{{ old('username') }}" placeholder="johndoe" required autofocus>
 
                             <p class="text-xs mt-1 text-grey-600">Note: your username is <b>not</b> your email address.</p>
 
@@ -53,7 +56,9 @@
                                 {{ __('Password') }}:
                             </label>
 
-                            <input id="password" type="password" class="appearance-none bg-grey-100 rounded w-full p-3 text-grey-700 focus:ring{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" placeholder="********" required>
+                            <input id="password" type="password"
+                                class="appearance-none bg-grey-100 rounded w-full p-3 text-grey-700 focus:ring{{ $errors->has('password') ? ' border-red-500' : '' }}"
+                                name="password" placeholder="********" required>
 
                             @if ($errors->has('password'))
                                 <p class="text-red-500 text-xs italic mt-4">
@@ -64,13 +69,15 @@
 
                         <div class="flex flex-wrap justify-between items-center">
                             <div class="mr-5 mt-4">
-                                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <input type="checkbox" name="remember" id="remember"
+                                    {{ old('remember') ? 'checked' : '' }}>
                                 <label class="text-sm text-grey-700 ml-3" for="remember">
                                     {{ __('Remember Me') }}
                                 </label>
                             </div>
                             @if (Route::has('password.request'))
-                                <a class="whitespace-nowrap no-underline text-sm mt-4" href="{{ route('password.request') }}">
+                                <a class="whitespace-nowrap no-underline text-sm mt-4"
+                                    href="{{ route('password.request') }}">
                                     {{ __('Forgot Username/Password?') }}
                                 </a>
                             @endif
@@ -79,7 +86,8 @@
                     </div>
 
                     <div class="px-6 md:px-10 py-4 bg-grey-50 border-t border-grey-100 flex flex-wrap items-center">
-                        <button type="submit" class="bg-cyan-400 w-full hover:bg-cyan-300 text-cyan-900 font-bold py-3 px-4 rounded focus:outline-none ml-auto">
+                        <button type="submit"
+                            class="bg-cyan-400 w-full hover:bg-cyan-300 text-cyan-900 font-bold py-3 px-4 rounded focus:outline-none ml-auto">
                             {{ __('Login') }}
                         </button>
                     </div>
