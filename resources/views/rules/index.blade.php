34e854dfd07d4d65e466aea922971f72a6ae90d2
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-18 22:27:14 +0700

    Update Rules

--- resources/views/rules/index.blade.php
+++ resources/views/rules/index.blade.php
@@ -4,6 +4,6 @@
     <div class="container py-8">
         @include('shared.status')
 
-        <rules :initial-rules="{{ json_encode($rules) }}" />
+        <rules :initial-rules='@json($rules)' />
     </div>
 @endsection
