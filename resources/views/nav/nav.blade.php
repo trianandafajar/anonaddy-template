c39d4812bcb52e64578990e027b0904c90105338
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-18 22:26:18 +0700

    Update Navbar

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -1,37 +1,33 @@
 @auth
-<aside class="bg-indigo-900 w-1/4 h-screen">
+<aside class="bg-indigo-900 w-1/4 h-screen flex flex-col justify-between">
     <div class="py-4">
-        <a href="{{ route('aliases.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Aliases
-        </a>
-        <a href="{{ route('recipients.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Recipients
-        </a>
-        <a href="{{ route('domains.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Domains
-        </a>
-        <a href="{{ route('usernames.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Usernames
-        </a>
-        <a href="{{ route('failed_deliveries.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Failed Deliveries
-        </a>
-        <a href="{{ route('rules.index') }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
-            Rules
-        </a>
+        @foreach ([
+            'aliases.index' => 'Aliases',
+            'recipients.index' => 'Recipients',
+            'domains.index' => 'Domains',
+            'usernames.index' => 'Usernames',
+            'failed_deliveries.index' => 'Failed Deliveries',
+            'rules.index' => 'Rules'
+        ] as $route => $label)
+            <a href="{{ route($route) }}" class="block px-6 py-3 text-white hover:bg-indigo-800">
+                {{ $label }}
+            </a>
+        @endforeach
     </div>
-    <div class="px-6 py-3 mt-auto">
-        <button class="badge badge-success">Upgrade</button>
-    </div>
+
     <div class="px-6 py-3">
-        <a href="{{ route('settings.show') }}" class="block text-white hover:bg-indigo-800">
+        <button class="badge badge-success mb-4">Upgrade</button>
+
+        <a href="{{ route('settings.show') }}" class="block text-white px-6 py-3 hover:bg-indigo-800">
             Settings
         </a>
-        <form action="{{ route('logout') }}" method="POST" class="block mt-3">
-            {{ csrf_field() }}
-            <input type="submit" class="w-full px-6 py-3 bg-transparent text-white hover:bg-indigo-800 cursor-pointer" value="{{ __('Logout') }}">
+
+        <form action="{{ route('logout') }}" method="POST" class="mt-3">
+            @csrf
+            <button type="submit" class="w-full px-6 py-3 text-left text-white hover:bg-indigo-800">
+                {{ __('Logout') }}
+            </button>
         </form>
     </div>
 </aside>
-
 @endauth
