aba7f836403a2a5565c1e694e541624cd302d8f4
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-18 22:26:09 +0700

    Update Recipient

--- resources/views/recipients/index.blade.php
+++ resources/views/recipients/index.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
+
 @section('breadcrumb')
-    a
+    a {{-- Ganti ini dengan breadcrumb jika memang diperlukan --}}
 @endsection
+
 @section('content')
     <div class="container py-8">
         @include('shared.status')
 
-        <recipients :user="{{json_encode($user)}}" :initial-recipients="{{json_encode($recipients)}}" :aliases-using-default="{{json_encode($aliasesUsingDefault)}}" :aliases-using-default-count="{{$aliasesUsingDefaultCount}}" domain="{{config('anonaddy.domain')}}" />
+        <recipients
+            :user='@json($user)'
+            :initial-recipients='@json($recipients)'
+            :aliases-using-default='@json($aliasesUsingDefault)'
+            :aliases-using-default-count='{{ $aliasesUsingDefaultCount }}'
+            domain="{{ config('anonaddy.domain') }}"
+        />
     </div>
 @endsection
