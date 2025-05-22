ffa852bbf286c02059f6a75e0c82e5077bd8bce3
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-22 14:58:21 +0700

    Update Show Domain Controller

--- app/Http/Controllers/ShowDomainController.php
+++ app/Http/Controllers/ShowDomainController.php
@@ -6,13 +6,15 @@ class ShowDomainController extends Controller
 {
     public function index()
     {
+        $domains = user()
+            ->domains()
+            ->with('defaultRecipient:id,email')
+            ->withCount('aliases')
+            ->latest()
+            ->get();
+
         return view('domains.index', [
-            'domains' => user()
-                ->domains()
-                ->with('defaultRecipient:id,email')
-                ->withCount('aliases')
-                ->latest()
-                ->get(),
+            'domains' => $domains,
         ]);
     }
 }
